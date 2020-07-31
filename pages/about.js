24458793086e5cee51b38a615057a2176be36b61
Freedom Evenden <free.eve@pm.me>
2020-07-30 19:17:28 -0700

    added initial css to about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
 import Nav from '../components/nav';
+import styles from '../styles/about.module.css'
 
 export default function About() {
   return (
-    <div>
+    <React.Fragment>
       <Nav />
-    </div>
+      <main className={styles.main}>
+        <h2>About Us</h2>
+      </main>
+    </React.Fragment>
   );
 }
