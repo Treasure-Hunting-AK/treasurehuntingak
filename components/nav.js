ddade1020ff33871591b82bb5c3baee882947a46
Freedom Evenden <free.eve@pm.me>
2020-07-30 18:45:07 -0700

    setting up basic styling of nav bar

--- components/nav.js
+++ components/nav.js
@@ -1,10 +1,13 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import styles from '../styles/nav.module.css';
 
 export default function Nav() {
   return (
-    <header>
-      <h2>Nav Page</h2>
+    <header className={styles.header}>
+      <Link href='/'>
+        <h1>Treasure Hunting AK</h1>
+      </Link>
     </header>
-  )
+  );
 }
