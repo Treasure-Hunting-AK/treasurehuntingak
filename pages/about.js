0e4d1bd47a0348fb0cd8fde1de5f61e9a9972e5a
Freedom Evenden <free.eve@pm.me>
2020-08-02 14:12:11 -0700

    updated about page content

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,25 @@
 import Nav from '../components/nav';
-import styles from '../styles/about.module.css'
+import styles from '../styles/about.module.css';
 
 export default function About() {
   return (
     <React.Fragment>
       <Nav />
       <main className={styles.main}>
-        <h2>About Us</h2>
+        <section className={styles.section}>
+          <h2>About Us</h2>
+          <p>
+            Hello and welcome to Treasure Hunting AK! We are an Alaska based
+            reselling shop that fully believes that one person’s trash is another
+            person’s treasure! are passionate about salvaging items, cleaning
+            them up, and getting them into the hands of people who see value in
+            them. We also love to reuse, recycle, and repurpose.
+          </p>
+          <p>
+            We are always on the hunt so head on over to our eBay shop and see
+            what treasures we have found!
+          </p>
+        </section>
       </main>
     </React.Fragment>
   );
