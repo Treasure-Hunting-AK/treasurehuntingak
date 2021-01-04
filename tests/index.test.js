60abc2dff87904b0e276e4f69fdeb1b946cd690b
f3ve <free.eve@pm.me>
2021-01-03 17:35:13 -0800

    expanded index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,24 @@
+/* eslint-disable no-undef */
 import { render, screen } from '@testing-library/react';
 import App from '../pages/index';
 
-describe('App', () => {
+describe('Index.js', () => {
   it('renders without crashing', () => {
     render(<App />);
     expect(
       screen.getByRole('heading', { name: 'Welcome to Treasure Hunting AK' })
     ).toBeInTheDocument();
+
+    expect(screen.getByText('Discover Alaska')).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: 'About Us' })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: 'Contact Us' })
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: 'Shop' })).toBeInTheDocument();
   });
 });
