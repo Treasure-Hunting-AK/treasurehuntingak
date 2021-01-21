9dfd0980f4a26b7fef12a4f2371213b742827a4b
f3ve <free.eve@pm.me>
2021-01-21 15:22:53 -0800

    removed custom background

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import '../styles/globals.css';
 import 'fontsource-roboto';
 import Nav from '../components/nav';
-import CustomBackground from '../components/global/custombackground';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Nav />
-      <CustomBackground />
       <Component {...pageProps} />
     </>
   );
