054624c4b8f8158d96a44fe470a7b6d4e0935f08
f3ve <free.eve@pm.me>
2021-01-21 20:03:24 -0800

    created new landing page layout

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,35 @@
 import Head from 'next/head';
-import { Container, Typography } from '@material-ui/core';
+import { Container, Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import RouterLink from '../components/global/RouterLink';
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    height: '100vh',
+  },
   header: {
-    width: '100%',
     flex: 1,
     display: 'flex',
     backgroundImage: 'url("/mainbackground.jpeg")',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     flexDirection: 'column',
-    paddingTop: 150,
     alignItems: 'center',
-    minHeight: '100vh',
     textAlign: 'center',
     padding: 20,
+    height: theme.breakpoints.up('sm') ? '50vh' : '',
     // color: '#fdfdfd',
   },
   link: {
     borderWidth: 2,
     borderColor: theme.palette.primary.main,
     borderStyle: 'solid',
-    margin: 10,
-    padding: 20,
-    transition: 'all 0.2s',
-    '&:hover': {
-      transform: 'scale(1.04)',
+    // padding: 20,
+    height: '50vh',
+    [theme.breakpoints.up('sm')]: {
+      height: '50vh',
     },
+    transition: 'all 0.2s',
   },
   box: {
     position: 'relative',
@@ -44,29 +45,29 @@ export default function App() {
         <title>Treasure Hunting AK</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container className={styles.header} disableGutters maxWidth="xl">
-        <Typography variant="h3" component="h2">
-          Treasure Hunting AK
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          className={styles.description}
-          align="center"
-        >
-          Discover Alaska
-        </Typography>
-        <section className={styles.grid}>
-          <RouterLink href="/about">
-            <div className={styles.link}>about</div>
-          </RouterLink>
-          <RouterLink href="/contact" variant="contained">
-            <div className={styles.link}>Contact</div>
-          </RouterLink>
-          <RouterLink href="/shop">
-            <div className={styles.link}>Shop</div>
-          </RouterLink>
-        </section>
-      </Container>
+      <Grid container spacing={0} className={styles.container}>
+        <Grid item className={styles.header} sm={6} xs={12}>
+          <Typography variant="h3" component="h2">
+            Treasure Hunting AK
+            <Typography
+              variant="subtitle1"
+              className={styles.description}
+              align="center"
+            >
+              Discover Alaska
+            </Typography>
+          </Typography>
+        </Grid>
+        <Grid item sm={6} xs={12} className={styles.link}>
+          <RouterLink href="/about">About</RouterLink>
+        </Grid>
+        <Grid item sm={6} xs={12} className={styles.link}>
+          <RouterLink href="/contact">Contact</RouterLink>
+        </Grid>
+        <Grid item sm={6} xs={12} className={styles.link}>
+          <RouterLink href="/shop">Shop</RouterLink>
+        </Grid>
+      </Grid>
     </>
   );
 }
