0425515b579f60d1c318d58e47cd908dbdfda65e
f3ve <free.eve@pm.me>
2021-01-23 18:36:06 -0800

    added some spacing between paragraphs on about page

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,9 @@ import { makeStyles } from '@material-ui/styles';
 const useStyles = makeStyles(() => ({
   main: {
     marginTop: 100,
+    '& p': {
+      marginBottom: 50,
+    },
   },
 }));
 
@@ -21,6 +24,7 @@ export default function About() {
         items, cleaning them up, and getting them into the hands of people who
         see value in them. We also love to reuse, recycle, and repurpose.
       </Typography>
+
       <Typography variant="body1" gutterBottom>
         We are always on the hunt so head over to our eBay shop and see what
         treasures we&apos;ve found!
